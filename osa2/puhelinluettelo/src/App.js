d9865bb3c121bf33760d8e1f76592d46c4d1fbf0
Ville Paananen <villepaananen@outlook.com>
2020-02-06 18:52:54 +0200

    still in 2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,33 +1,40 @@
 import React, { useState } from "react";
 
-const Button = () => {
-  return <button type="submit">add</button>;
-};
-
-const Number = ({ name, number }) => {
-  return (
-    <div>
-      {name} {number}
-    </div>
-  );
+const Name = ({ name }) => {
+  return <div>{name}</div>;
 };
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNameChange = event => {
+    setNewName(event.target.value);
+  };
+
+  const addName = event => {
+    event.preventDefault();
+    const nameObject = {
+      key: newName
+    };
+
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+    console.log(persons);
+  };
+
+  const rows = () =>
+    persons.map(name => <Name key={name.content} id={name.id} />);
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <Button />
-        </div>
+      <form onSubmit={addName}>
+        <input value={newName} onChange={handleNameChange}></input>
+        <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
+      <ul>{rows()}</ul>
     </div>
   );
 };
